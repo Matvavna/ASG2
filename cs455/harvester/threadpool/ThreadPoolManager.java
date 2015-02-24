866c857ec1acdcb51150e366a007b90ee26176de
wHome <wbarras@rams.colostate.edu>
2015-02-24 14:47:35 -0700

    Worker should play with thread pool now. ThreadPoolManager still needs to have wait/notify on tasksToComplete implemented

--- cs455/harvester/threadpool/ThreadPoolManager.java
+++ cs455/harvester/threadpool/ThreadPoolManager.java
@@ -6,45 +6,72 @@ package cs455.harvester.threadpool;
  *Maintains a queue of worker threads and a queue of tasks, and assigns the tasks to the workers
  */
 
- import cs455.harvester.threadpool.Worker;
+import cs455.harvester.threadpool.Worker;
+import cs455.harvester.task.Task;
 
- import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
-public class ThreadPoolManager{
+
+public class ThreadPoolManager implements Runnable{
 
   private final int numberOfThreads;
-  private ConcurrentLinkedQueue<Worker> availableWorkers;
+
+  //This holds references to the Threads running available workers
+  //When there is a task to be done, the worker grabs it, and removes itself from the queue
+  //When it completes that task, it places itself back at the end of the queue
+  private final ConcurrentLinkedQueue<Thread> availableWorkers = new ConcurrentLinkedQueue<Thread>();
+
+  //Hold references to tasks waiting to be completed
+  //When there is a task to be done, it is grabbed by the next available worker
+  private final ConcurrentLinkedQueue<Task> tasksToComplete = new ConcurrentLinkedQueue<Task>();
 
 
   public ThreadPoolManager(int _numberOfThreads){
     numberOfThreads = _numberOfThreads;
-
-    this.initWorkers(this.numberOfThreads);
-
-    this.startWorkers();
   }//End constructor
 
 
-  //Initializes the availableWorkers queue with the specified amount of Workers
-  private void initWorkers(int threadCount){
-    this.availableWorkers = new ConcurrentLinkedQueue<Worker>();
+  public void run(){
+    //Fill availableWorkers with workerThreads
+    this.initWorkers(this.numberOfThreads);
+    //Start all the workers
+    this.startWorkers();
+  }
 
+
+  //Initializes the proper amount of Workers, and assigns each one to a Thread
+  //The Threads are placed in the availableWorkers Queue
+  private void initWorkers(int threadCount){
     //Add the appropriate number of Workers to the Queue
     for(int i = 0; i < threadCount; i++){
-      this.availableWorkers.add(new Worker());
+      String workerName = "Worker:" + i;//Generate a unique name for each thread
+      Thread workerThread = new Thread(new Worker(this, tasksToComplete), workerName);
+      this.availableWorkers.add(workerThread);
     }
 
-    //Queue should now contain the proper numbers of workers, but they have not yet been started
+    //Queue should now contain the proper numbers of threads, but they have not yet been started
   }//End initWorkers
 
   //Starts each of the Workers in the queue
   private void startWorkers(){
-    for(Worker workerToStart : this.availableWorkers){
-      workerToStart.run();
+    for(Thread workerThreadToStart : this.availableWorkers){
+      workerThreadToStart.start();
     }
-
   }//End startWorkers
+
+  private void addTask(Task task){
+    this.tasksToComplete.add(task);
+  }//End addTask
+
+  public Task getTask()throws NoSuchElementException{
+    //This needs to be synchronized
+    //If not, then two Workers could both recieve the last Task in the Queue
+    synchronized(tasksToComplete){
+      return tasksToComplete.remove();
+    }
+  }//End getTask
 
 
   //This main is for the purposes of testing this class only
--- cs455/harvester/threadpool/Worker.java
+++ cs455/harvester/threadpool/Worker.java
@@ -6,14 +6,81 @@ package cs455.harvester.threadpool;
  *Assigned tasks by the ThreadPoolManager
  */
 
+import cs455.harvester.task.Task;
+import cs455.harvester.threadpool.ThreadPoolManager;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Worker implements Runnable{
 
+	/*
+	 *This is the manager that created this Worker
+	 *Need a reference to this so we can pass back new tasks
+	 */
+	private final ThreadPoolManager manager;
+
+	/*
+	 *Don't ever modify this. It should only be wait/notify-ed on
+	 *Wait when there's nothing to take out of it.
+	 *Maybe notify if adding is implemented in this class(That might all be done in the manager)
+	 */
+	private final ConcurrentLinkedQueue<Task> tasksToComplete;
+
+	public Worker(ThreadPoolManager _manager, ConcurrentLinkedQueue<Task> _tasksToComplete){
+		manager = _manager;
+		tasksToComplete = _tasksToComplete;
+	}//End constructor
 
 
-
+	//Runs until it is given the task to kill itself
 	public void run(){
-		System.out.println("I'm a lumberjack and I'm OK");
+		while(true){
+			//Only one Worker can be looking at tasksToComplete at a time
+			//Otherwise, isEmpty could end up in an inconsistent state
+			synchronized(tasksToComplete){
+				if(tasksToComplete.isEmpty()){
+					try{
+						tasksToComplete.wait();
+					}catch(InterruptedException exception){
+						System.out.println("Worker: Interrupted");
+						System.out.println(exception);
+					}
+				}
+			}//End sychronized block
+
+			//I don't think this bit needs to be synchronized, because ThreadPoolManager
+			// Will handle synchronizing requests for tasks
+			Task currentTask;
+
+			//Grab a task from the manager
+			//This will be null if the list is actually empty
+			currentTask = this.requestTask();
+
+			//Make sure a task was actually returned
+			//If currenTask is null, that means no task was returned from the manager
+			//The continue statement puts is back at the top of the while,
+			//  and we'll wait on the task list again
+			if(currentTask == null) continue;
+
+
+			currentTask.execute();
+		}
 	}//End run
+
+	private Task requestTask(){
+		Task newTask = null;
+		try{
+			newTask = this.manager.getTask();
+		}catch(NoSuchElementException exception){
+			//Hopefully this branch is never ever reached
+			//If it is, then a concurrency issue is causing workers to think the task
+			// list has elements when it's actually empty!
+			System.out.println("Worker: No task to recieve from ThreadPoolManager");
+			System.out.println(exception);
+		}
+		return newTask;
+	}
 
 
 }//End class
